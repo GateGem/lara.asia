f1b81a47a172052ab464b6c4f05c16d944ca3fd8
Nguyen Van Hau <nguyenvanhau.soft@gmail.com>
2022-10-05 10:29:41 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    images: {
-      unoptimized: true
-    }
-  }
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
 }
 
 module.exports = nextConfig
